9d829aa53c35fc96813c39ee82d4cc0b9e19f808
Shiraz Besrour <shiraz.besrour@gmail.com>
2023-03-28 22:43:48 +0000

    sonar

--- src/main/java/com/esprit/examen/repositories/FournisseurRepository.java
+++ src/main/java/com/esprit/examen/repositories/FournisseurRepository.java
@@ -1,5 +1,6 @@
 package com.esprit.examen.repositories;
 
+import com.esprit.examen.entities.DTO.FournisseurDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.esprit.examen.entities.Fournisseur;
