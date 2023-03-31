934e0e3b9db79b4654af9eb912569c66a31de9e9
Ahlem <jbeliahlem2@gmail.com>
2023-03-31 12:13:58 +0100

    test without mock

--- src/test/java/com/esprit/examen/test/ProduitServiceImplMockTest.java
+++ src/test/java/com/esprit/examen/test/ProduitServiceImplMockTest.java
@@ -14,7 +14,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Example;
+
 
 
 
